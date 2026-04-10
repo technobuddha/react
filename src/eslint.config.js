078d192643845ce1b6b254cc40dd38b3b6a91c13
Phil Hill <phil@technobuddha.com>
2026-04-10 05:23:55 -0400

    publish

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -5,14 +5,10 @@
 import { lint } from '@technobuddha/project';
 
 export default lint(
-  {
-    files: ['**/*.ts'],
-    ignores: ['@types/**/*', 'components/datagrid/@types/**/*'],
-    typescript: true,
-  },
+  { files: ['**/*.ts'], ignores: ['components/datagrid/@types/**/*'], typescript: true },
   {
     files: ['**/*.tsx'],
-    ignores: ['@types/**/*', 'components/datagrid/@types/**/*'],
+    ignores: ['components/datagrid/@types/**/*'],
     typescript: true,
     react: true,
   },
