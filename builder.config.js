51233e6d3c914759bdcfd00de75fc3669e30b37e
Phil Hill <phil@technobuddha.com>
2026-04-20 07:26:06 -0400

    publish

--- builder.config.js
+++ builder.config.js
@@ -17,7 +17,7 @@ const config = {
       { build: 'default' },
       {
         name: 'Version',
-        command: 'yarn version patch',
+        command: 'yarn version prerelease',
       },
       {
         name: 'Publish',
